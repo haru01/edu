96f7a85be49f2d708597e091d38e54882b1866d0
eiji.ienaga <eiji.ienaga@gmail.com>
2011-10-13 13:19:43 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seminar::Application.config.secret_token = 'b371d35de9dae0d6e37313af804165d6b6b5fc57efc38a6c596fd4216a3c1c45d7c81b28a3656dcdddc491404aca54f2822105171218828a07127902c466784a'
