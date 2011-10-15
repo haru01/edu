63c32d24447b4bed5bfeafa3608834f9368ec788
eiji.ienaga <eiji.ienaga@gmail.com>
2011-10-15 10:20:18 +0900

    授業情報が取得できていることのテストを増やした

--- features/step_definitions/course_step.rb
+++ features/step_definitions/course_step.rb
@@ -4,23 +4,34 @@ require 'fabrication/cucumber'
 
 World(Rack::Test::Methods)
 
-前提 /^授業が登録されている$/ do |table|
+Given /^授業が登録されている$/ do |table|
   table.hashes.each do | hash |
     Fabricate(:course,
-      from_date: Date.strptime(hash[:from_date], "%Y-%m-%d"))
+      from_date: Date.strptime(hash["開催日"], "%Y-%m-%d"),
+      location: hash["場所"],
+      number: hash["人数"].to_i)
   end
 end
 
-前提 /^授業の一覧取得APIにアクセスしている$/ do
+When /^授業の一覧取得APIにアクセスしている$/ do
   header 'Accept', 'application/json'
   header 'Content-Type', 'application/json'
   get courses_path
 end
 
-ならば /^登録された授業の一覧が開始順に取得できること:$/ do |table|
+Then /^登録された授業の一覧が開催日順に取得できること:$/ do |table|
   page = JSON.parse(last_response.body)
   acutal = page.map { |node| node["from_date"] }
-  exptect = table.hashes.map { | hash | hash[:from_date] }
+  exptect = table.hashes.map { | hash | hash["開催日"] }
 
   acutal.should == exptect
+end
+
+Then /^授業情報が取得できていること$/ do |table|
+  actual = JSON.parse(last_response.body)[0]
+  exptect = table.hashes[0]
+
+  actual["from_date"].should == exptect["開催日"]
+  actual["location"].should == exptect["場所"]
+  actual["number"].should == exptect["人数"].to_i
 end
