97b7a1566a4e8e03d156bc25b413c6d4c220484e
eiji.ienaga <eiji.ienaga@gmail.com>
2011-10-16 09:17:49 +0900

    add expect, only

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,8 @@ class CoursesController < ApplicationController
   respond_to :json
   def index
     @courses = Course.order("from_date")
-    respond_with(@courses,{include: :main_lecturer})
+    respond_with(@courses,
+                         include: { main_lecturer: { only: :name } },
+                         except: [:created_at, :updated_at] )
   end
 end
